e8322d87137b262ae02bbc0f970ca5fd9f9450da
Pratish Chavan <chavanpratish@live.com>
2019-02-08 21:09:36 +0530

    Update phpfsplit.php

--- split and merge/phpfsplit.php
+++ split and merge/phpfsplit.php
@@ -1,3 +1,4 @@
+
 <?php
 
 
@@ -20,7 +21,7 @@ function fsplit($file,$buffer=1024){
     
     for($i=0;$i<$parts;$i++){
         //read buffer sized amount from file
-        $file_part = fread($file_handle, $buffer);
+         $file_part = fread($file_handle, $buffer);
         //the filename of the part
         $file_part_path = $store_path.$file_name.".part$i"; // files are attached with parts and number of part
         //open the new file [create it] to write
